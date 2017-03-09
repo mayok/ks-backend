542e4d0b4500b2866df39feb30cb79b5639e5326
Naohiro IWAMOTO <naodai24@gmail.com>
2017-03-09 14:46:38 +0900

    Update postgres container

--- postgres/init-user-db.sh
+++ postgres/init-user-db.sh
@@ -12,9 +12,3 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO summpy;
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO summpy;
 EOSQL
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" \
-     -c "CREATE LANGUAGE plpythonu" summpy
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" \
-     -f /tmp/summpy.sql summpy
