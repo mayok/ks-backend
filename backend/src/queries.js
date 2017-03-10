da93034c3eef3f98cdfdc46f68575d86f393b903
Naohiro IWAMOTO <naodai24@gmail.com>
2017-03-10 16:10:11 +0900

    WIP: getContent の一連の処理を雑に実装 (2)
      - xml をテキストに変換する関数の実装
      - 動作確認

--- backend/src/queries.js
+++ backend/src/queries.js
@@ -6,6 +6,52 @@ const connectionString =
   `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:5432/${process.env.PGDATABASE}`;
 const db = pgp(connectionString);
 
+
+const fetchTextFrom = url => new Promise((resolve, reject) => {
+  const req = http.get(url, (res) => {
+    if (res.statusCode < 200 || res.statusCode > 299) {
+      reject(new Error(
+        `Failed to load page, status code: ${res.statusCode}`,
+      ));
+    }
+
+    const body = [];
+    res.on('data', chunk => body.push(chunk));
+    res.on('end', () => resolve(body.join('')));
+  });
+  req.on('error', err => reject(err));
+});
+
+const parse = xml => new Promise((resolve, reject) => {
+    // parse xml to json or plain text
+    // resolve(text);
+});
+
+const summarize = (text, options) => new Promise((resolve, reject) => {
+  const postData = {
+    text,
+    sent_limit: options.sent_limit,
+  };
+  const params = {
+    host: options.url,
+    port: 80,
+    path: '/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length,
+    },
+  };
+  const req = http.request(params, (res) => {
+    const body = [];
+    res.on('data', chunk => body.push(chunk));
+    res.on('end', () => resolve(body.join('')));
+  });
+  req.on('error', err => reject(err));
+  req.write(postData);
+  req.end();
+});
+
 export const getTitles = (req, res, next) => {
   db.any('select title from record')
     .then((data) => {
@@ -19,83 +65,39 @@ export const getTitles = (req, res, next) => {
 };
 
 export const getContent = (req, res, next) => {
-  // 流れ
-  // 1. db.one select summarized text, return
-  // 2. fetch text from API
-  // 3. parse xml to plain text or json
-  // 4. summarize
-  // 5. return summarized text to client
-  // 6. insert into record
-
   // id ではなくて, date に変更するかも
   const id = parseInt(req.params.id, 10);
-  db.one('select content from record where id = $1', id)
+  db.oneOrNone('select content from record where id = $1', id)
     .then((data) => {
-      res.status(200)
-        .json({
-          status: 'success',
-          data,
-        });
+      if (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data,
+          });
+      } else {
+        // TODO: URL はどこか別の場所で定義する
+        const URL = 'example.com';
+        fetchTextFrom(URL)
+          .then(xml => parse(xml))
+          .then(text => summarize(text, {
+            url: URL,
+            sent_limit: 3,
+          }))
+          .then(json => res.status(200)
+                           .json({
+                             status: 'success',
+                             json,
+                           }),
+          )
+          .then(
+            db.none('insert into record(title, content) values($1, $2)',
+                    ['title', 'content'])
+              .then()
+              .catch(err => next(err)),
+          )
+          .catch(err => console.log(err));
+      }
     })
-    .catch((err) => {
-      // next(err);
-
-      // 以下、fetch, parse 等は catch の中に書くことじゃないと思う。
-      // 解決策が今は思いつかないため、残しておく
-
-      // 2. fetch text from API
-      //   HTTP GET
-      const URL = 'example.com';
-      http.get(URL, (response) => {
-        let body = '';
-        response.setEncoding('utf8');
-
-        response.on('data', (chunk) => {
-          body += chunk;
-        });
-
-        response.on('end', (r) => {
-          // 3. parse xml to json
-          // json = XML.parse(body);
-          // console.log(json);
-        });
-      }).on('error', e => console.log(e.message));
-
-      // 4. summarize
-      //    HTTP POST
-      //      return summarized data
-      const postData = {
-        text: 'text',
-        sent_limit: '3',
-      };
-      const options = {
-        host: URL,
-        port: 80,
-        path: '/',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': postData.length,
-        },
-      };
-      const request = http.requestuest(options, (response) => {
-        response.setEncoding('utf8');
-        response.on('data', (chunk) => {
-          console.log(chunk);
-          // res.status(200)
-          //    .json({
-          //      status: 'success',
-          //      chunk,
-          //    });
-        });
-      });
-      request.on('error', e => console.log(e.message));
-      // request.write(json)
-      request.end();
-
-      // db.none('insert into record(title, content) values($1, $2)',
-      //         ['title', 'content'])
-      //   .then()
-      //   .catch(err => next(err));
-    });
+    .catch((err) => { next(err); });
 };
